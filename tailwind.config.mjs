716c6cb47be7acf60c3e44f5f6f83a385861aa7f
Matteo Tagliatti <matteotagliatti@gmail.com>
2024-05-10 10:40:27 +0200

    add all

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,20 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
-export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+
+      colors: {
+        background: "#050505",
+        bubble: "#fafcff14",
+        para: "#ffffffcc",
+      },
+    },
+  },
+  plugins: [],
+};
